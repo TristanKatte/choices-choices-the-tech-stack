bce0ed9dfd7b80c0adff307b1f75dc023e5ac494
TristanKatte <tristankatte@hotmail.com>
2024-10-18 02:36:27 +0200

    custom server voor chatten

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import Chat from "./components/chat.js";
+
+
 export default function Home() {
   return (
     <div className={styles.page}>
